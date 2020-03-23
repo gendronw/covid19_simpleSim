81916d7ae2f73dcaccca65c80c0fb215506a6f2f
William Gendron <gendronw@hotmail.com>
2020-03-22 22:12:44 -0400

    started to include normal hospital bed need…needs debug because bed need never go down

--- covid19Sim.py
+++ covid19Sim.py
@@ -46,17 +46,28 @@ date_index = 0
 
 #-------------health system variable-------------
 hlt_days_to_get_to_hospital = 10
+hlt_days_normalbed_stay = 7
 hlt_ratio_people_need_hospital = 0.10
 hlt_ratio_people_need_ICU = 0.01
 hlt_ratio_people_die = 0.006
 
-hlt_normalBed = np.array([10000])
-hlt_ICU_bed = np.array([1000])
+
+hlt_normalBed_available = 10000
+hlt_ICU_bed_available = 1000
+
+hlt_newNormalBed_need = np.array([0])
+hlt_ICU_Bed_need = np.array([0])
+
+hlt_normalBed_inUsed = np.array([0])
+hlt_ICU_bed_inUsed = np.array([0])
+
+hlt_normalBed_overflow = np.array([0])
+hlt_ICU_bed_overflow = np.array([0])
 
 dayIndex_toCountDead = 20
 
 
-while (date_index < 365):
+while (date_index < 550):
     #increment date
     dates = np.append(dates, [dates[date_index]+timedelta(days=1)])
 
@@ -68,9 +79,6 @@ while (date_index < 365):
 
     #calculate cured people
     dRdt = np.append(dRdt, gamma * I[date_index])
-
-
-
 
     #calculate sum of cured and infected
     S = np.append(S, [S[date_index] + dSdt[date_index]])
@@ -100,11 +108,32 @@ while (date_index < 365):
     #remove dead from infected
     I[date_index] = I[date_index] - dDdt[date_index]
 
+    #calculate used normal hospital bed
+    if date_index > hlt_days_to_get_to_hospital:
+        hlt_newNormalBed_need = np.append(hlt_newNormalBed_need, [infected_daily[date_index-hlt_days_to_get_to_hospital]*hlt_ratio_people_need_hospital])
+        #removed saved person
+        if date_index > (hlt_days_normalbed_stay + hlt_days_to_get_to_hospital):
+            hlt_newNormalBed_need[date_index] = hlt_newNormalBed_need[date_index]-hlt_newNormalBed_need[date_index-hlt_days_normalbed_stay]
+
+        hlt_normalBed_inUsed = np.append(hlt_normalBed_inUsed, [hlt_normalBed_inUsed[date_index-1] + hlt_newNormalBed_need[date_index]])
+    else:
+        hlt_normalBed_inUsed = np.append(hlt_normalBed_inUsed, [0])
+        hlt_newNormalBed_need = np.append(hlt_newNormalBed_need, [0])
+
+
+
+    if hlt_normalBed_inUsed[date_index] > hlt_normalBed_available:
+        hlt_normalBed_overflow = np.append(hlt_normalBed_overflow, [hlt_normalBed_inUsed[date_index] - hlt_normalBed_available])
+        hlt_normalBed_inUsed[date_index] = hlt_normalBed_available
+    else:
+        hlt_normalBed_overflow = np.append(hlt_normalBed_overflow, [0])
+
+
 
 
 
 #create Covid-19 figure
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True)
 fig.suptitle('Covid-19 Quebec simulation')
 ax1.plot_date(dates, I, label='infected')
 ax1.plot_date(dates, R, label='recovered')
@@ -118,9 +147,11 @@ ax2.xaxis.set_tick_params(rotation=90, labelsize=10)
 ax2.legend()
 
 ax3.bar(dates, dIdt, label='Infected variation')
+ax3.plot_date(dates, infected_daily, color='k', label='Newly infected')
 ax3.legend()
 
-ax4.plot_date(dates, infected_daily, label='Newly infected')
+
+ax4.plot_date(dates, D, label='dead')
 ax4.legend()
 
 plt.tight_layout()
@@ -129,16 +160,16 @@ plt.show()
 
 
 #create health care figure
-fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, sharex=True)
 fig.suptitle('Health care Covid-19 Quebec simulation')
-ax1.plot_date(dates, I, label='infected')
-ax1.plot_date(dates, R, label='recovered')
+ax1.plot_date(dates, hlt_normalBed_inUsed, label='normal hospital Bed in use')
+ax1.plot_date(dates, hlt_normalBed_overflow, label='normal bed overflow')
 ax1.legend()
 #graph_infectedPersons.xaxis.set_major_locator(loc)
 #ax1.xaxis.set_major_formatter(formatter)
 ax1.xaxis.set_tick_params(rotation=90, labelsize=10)
 
-ax2.plot_date(dates, R_value, label='rvalue')
+ax2.plot_date(dates, hlt_newNormalBed_need, label='new normal bed need')
 ax2.xaxis.set_tick_params(rotation=90, labelsize=10)
 ax2.legend()
 
