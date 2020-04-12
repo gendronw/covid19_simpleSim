28d67e764c35766ee7663a5abc11a32a2c4e19cf
William Gendron <gendronw@hotmail.com>
2020-04-11 22:47:32 -0400

    new scenario with dynamic R0

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Python covid-19 simulator based on SIR model. model number based on Quebec provi
 
 
 Need to do :
-* Implement hospitalised person
-* Implement hospital system overflow
-* implement dead person count
+* ~~Implement hospitalised person~~
+* ~~Implement hospital system overflow~~
+* ~~implement dead person count~~
 * Implement social_distancing/variable R0 value
 * Implement social distancing On/off depending on hospital system overflow (create sawtooth response)
--- covid19Sim_Scenario1_dynamicR0.py
+++ covid19Sim_Scenario1_dynamicR0.py
@@ -9,7 +9,10 @@ formatter = DateFormatter('%m/%d/%y')
 
 # -------set simulation parameter--------------
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+beta_original = 0.10
+beta_to_reach = 0.10
 beta = 0.10
+
 daysInfectionIsActive = 26
 gamma = 1/daysInfectionIsActive
 
@@ -17,7 +20,7 @@ gamma = 1/daysInfectionIsActive
 N = 8000000
 
 # initial variable value for simulation
-dates = np.array([datetime.date(2020, 3, 1)])
+dates = np.array([datetime.date(2020, 2, 10)])
 
 # S is suceptible population
 # I is infected
@@ -29,7 +32,7 @@ dIdt = np.array([0])
 dRdt = np.array([0])
 dDdt = np.array([0])
 
-I = np.array([12])
+I = np.array([1000])
 R = np.array([0])
 S = np.array([N-I[0]-R[0]])
 D = np.array([0])
@@ -46,10 +49,10 @@ hlt_days_to_get_to_hospital = 10
 hlt_days_normalbed_stay = 10
 hlt_ratio_people_need_hospital = 0.10
 hlt_ratio_people_need_ICU = 0.01
-hlt_ratio_people_die = 0.006
+hlt_ratio_people_die = 0.02
 
-hlt_normalBed_available = 10000
-hlt_ICU_bed_available = 1000
+hlt_normalBed_available = 8000
+hlt_ICU_bed_available = 600
 
 hlt_newNormalBed_need = np.array([0])
 hlt_ICU_Bed_need = np.array([0])
@@ -62,12 +65,61 @@ hlt_ICU_bed_overflow = np.array([0])
 
 dayIndex_toCountDead = 20
 
-# -------------dynamic R0 rate -------------
+# -------------dynamic R0 : social distancing rating -------------
+
+construction_workers = 0.05
+work_From_Home = 0.15
+School_And_KinderGarden = 0.20
+cegep_And_University = 0.10
+two_Meter_Rule = 0.25
+sunday_Close_Shop = 0.03
+essential_Shop = 0.22
+
+begin_Social_Distancing = datetime.date(2020, 3, 14)
+
+start_Construction = datetime.date(2020, 4, 27)
+start_school_before_summer = datetime.date(2020, 5, 19)
+close_school_summer = datetime.date(2020, 6, 20)
+start_school_september = datetime.date(2020, 8, 24)
 
 
-while (date_index < 550):
+#socail activity of 1 mean a population without social distancing
+socialActivity = np.array([1])
+
+while (date_index < 300):
     # increment date
     dates = np.append(dates, [dates[date_index]+timedelta(days=1)])
+
+    #look to start social distancing
+    if(dates[date_index+1] == begin_Social_Distancing):
+        socialActivity = np.append(socialActivity, socialActivity[date_index]-
+                                   (construction_workers + work_From_Home + School_And_KinderGarden
+                                    + cegep_And_University + two_Meter_Rule + sunday_Close_Shop))
+        beta_to_reach = socialActivity[date_index+1] * beta_original
+    elif(dates[date_index+1] == close_school_summer):
+        socialActivity = np.append(socialActivity, socialActivity[date_index]-School_And_KinderGarden)
+        beta_to_reach = socialActivity[date_index+1] * beta_original
+
+    #look to remove social distancing
+    elif(dates[date_index+1] == start_Construction):
+        socialActivity = np.append(socialActivity, socialActivity[date_index] + construction_workers)
+        beta_to_reach = socialActivity[date_index + 1] * beta_original
+    elif (dates[date_index + 1] == start_school_before_summer):
+        socialActivity = np.append(socialActivity, socialActivity[date_index] + School_And_KinderGarden)
+        beta_to_reach = socialActivity[date_index + 1] * beta_original
+    elif (dates[date_index + 1] == start_school_september):
+        socialActivity = np.append(socialActivity, socialActivity[date_index] + School_And_KinderGarden + cegep_And_University)
+        beta_to_reach = socialActivity[date_index + 1] * beta_original
+
+    else:
+        socialActivity = np.append(socialActivity, socialActivity[date_index])
+
+
+    #move slowly beta to wanted number
+    if beta < beta_to_reach:
+        beta = beta + 0.002
+    elif beta > beta_to_reach:
+        beta = beta - 0.002
 
     # calculate change in suceptible people
     dSdt = np.append(dSdt, -beta * S[date_index] * I[date_index] / N)
@@ -88,12 +140,12 @@ while (date_index < 550):
     date_index = date_index + 1
 
     # calculate daily infected
-    if date_index > daysInfectionIsActive:
-        infected_daily = np.append(infected_daily, [(dIdt[date_index] + dRdt[date_index])])
-        if infected_daily[date_index] < 0:
-            infected_daily[date_index] = 0
-    else:
-        infected_daily = np.append(infected_daily, [dIdt[date_index]])
+    #if date_index > daysInfectionIsActive:
+    infected_daily = np.append(infected_daily, [(dIdt[date_index] + dRdt[date_index])])
+    #    if infected_daily[date_index] < 0:
+    #        infected_daily[date_index] = 0
+    #else:
+    #    infected_daily = np.append(infected_daily, [dIdt[date_index]])
 
     # ------calculate used normal hospital bed-------
     if date_index > hlt_days_to_get_to_hospital:
@@ -146,9 +198,14 @@ ax1.plot_date(dates, R, label='recovered')
 ax1.xaxis.set_tick_params(rotation=90, labelsize=10)
 ax1.legend()
 
-ax2.plot_date(dates, R_value, label='R0_value')
+ax2.plot_date(dates, R_value, label='R0_value & social activity')
 ax2.xaxis.set_tick_params(rotation=90, labelsize=10)
 ax2.legend()
+
+ax2_twin = ax2.twinx()
+ax2_twin.set_ylabel('social', color='r')  # we already handled the x-label with ax1
+ax2_twin.plot_date(dates, socialActivity, color = 'r')
+
 
 ax3.bar(dates, dIdt, label='Infected variation')
 ax3.plot_date(dates, infected_daily, color='k', label='Newly infected')
